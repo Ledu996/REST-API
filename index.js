a3eb58cfd824cc3de626731bb6cf181225e5c2db
Ledu996 <dusan.dorcol@hotmail.com>
2021-03-18 18:55:16 +0100

    Kreiranje servera, uzimanje podataka iz request-a

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Hello World!');
+// Zavisisnosti
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+
+const server = http.createServer((req, res) => {
+    
+    const parsedURL = url.parse(req.url, true);
+
+    const path = parsedURL.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    const method = req.method.toLocaleLowerCase();
+
+    const queryStringObject = parsedURL.query;
+
+    const headers = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        console.log(buffer);
+        res.end('Hello world!');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Servej je pokrenut na portu 3000');
+});
