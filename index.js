b332aa8d6424a15fd473ca6c308bfdeaba87ad62
Ledu996 <dusan.dorcol@hotmail.com>
2021-03-21 19:49:40 +0100

    Dodavanje ping rute, handler-a, konfiguracije i pocetak rada sa podacima

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
 
 const server = http.createServer((req, res) => {
     
@@ -24,11 +25,48 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
         buffer += decoder.end();
 
-        console.log(buffer);
-        res.end('Hello world!');
+        const choosenHandler = typeof router[trimmedPath] !== "undefined" ? router[trimmedPath] : handlers.notFound;
+
+        const data = {
+            trimmedPath: trimmedPath,
+            queryStringObject: queryStringObject,
+            method: method,
+            headers: headers,
+            payload: buffer
+        };
+
+        choosenHandler(data, (statusCode, payload) => {
+            // osigurati da je status kod broj
+            statusCode = typeof statusCode == 'number' ? statusCode : 200;
+            // osigurati da je payload objekat
+            payload = typeof payload == 'object' ? payload : {};
+
+            const payloadString = JSON.stringify(payload);
+
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            console.log('Response vraca ' + statusCode + " " + payloadString);
+        });
+
     });
 });
 
-server.listen(3000, () => {
-    console.log('Servej je pokrenut na portu 3000');
+server.listen(config.port, () => {
+    console.log(`Server je pokrenut na portu ${config.port} i ime okruzenja je ${config.envName}`);
 });
+
+const handlers = {};
+
+handlers.ping = (data, callback) => {
+    callback(200);
+};
+
+handlers.notFound = (data, callback) => {
+    callback(404);
+};
+
+const router = {
+    'ping': handlers.ping
+}
