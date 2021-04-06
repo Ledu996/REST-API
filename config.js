6be896573d1a76e132f7a150a6c179b39d7a3e94
Ledu996 <dusan.dorcol@hotmail.com>
2021-04-06 19:03:15 +0200

    Dodati hendleri za check-ove

--- config.js
+++ config.js
@@ -4,14 +4,16 @@ environments.staging = {
     'httpPort': 3000,
     'httpsPort': 3001,
     'envName': 'staging',
-    'hashingSecret': 'ovoJeNekiSalt'
+    'hashingSecret': 'ovoJeNekiSalt',
+    'maxChecks': 5
 };
 
 environments.production = {
     'httpPort': 5000,
     'httpsPort': 5001,
     'envName': 'production',
-    'hashingSecret': 'ovoJeNekiSalt'
+    'hashingSecret': 'ovoJeNekiSalt',
+    'maxChecks': 5
 };
 
 const currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLocaleLowerCase() : '';
