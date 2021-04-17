afe3d9d3be3aa2ee88362fc1396d230975225c10
Ledu996 <dusan.dorcol@hotmail.com>
2021-04-17 18:22:36 +0200

    Refaktorisanje i worker-i

--- index.js
+++ index.js
@@ -1,89 +1,18 @@
-// Zavisisnosti
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
-const fs = require('fs');
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-helpers.sendTwilioSMS('5005550333', 'Hello World!', (err) => {
-    console.log('Greska je ovo: ' + err);
-});
+const app = {};
 
-const httpServer = http.createServer((req, res) => {
-    unifiedServer(req, res);
-});
+app.init = () => {
 
-httpServer.listen(config.httpPort, () => {
-    console.log(`Server je pokrenut na portu ${config.httpPort} i ime okruzenja je ${config.envName}`);
-});
+    //pokretanje servera
+    server.init();
+    
+    //pokretanje worker-a
+    workers.init();
 
-const httpsServerOptions = {
-    'key': fs.readFileSync('./https/key.pem'),
-    'cert': fs.readFileSync('./https/cert.pem')
-};
-
-const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
-    unifiedServer(req, res);
-});
-
-httpsServer.listen(config.httpsPort, () => {
-    console.log(`Server je pokrenut na portu ${config.httpsPort} i ime okruzenja je ${config.envName}`);
-});
-
-const unifiedServer = (req, res) => {
-    const parsedURL = url.parse(req.url, true);
-
-    const path = parsedURL.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-    const method = req.method.toLocaleLowerCase();
-
-    const queryStringObject = parsedURL.query;
-
-    const headers = req.headers;
-
-    const decoder = new StringDecoder('utf-8');
-    let buffer = '';
-    req.on('data', (data) => {
-        buffer += decoder.write(data);
-    });
-    req.on('end', () => {
-        buffer += decoder.end();
-
-        const choosenHandler = typeof router[trimmedPath] !== "undefined" ? router[trimmedPath] : handlers.notFound;
-
-        const data = {
-            trimmedPath: trimmedPath,
-            queryStringObject: queryStringObject,
-            method: method,
-            headers: headers,
-            payload: buffer
-        };
-
-        choosenHandler(data, (statusCode, payload) => {
-            // osigurati da je status kod broj
-            statusCode = typeof statusCode == 'number' ? statusCode : 200;
-            // osigurati da je payload objekat
-            payload = typeof payload == 'object' ? payload : {};
-
-            const payloadString = JSON.stringify(payload);
-
-            res.setHeader('Content-Type', 'application/json');
-            res.writeHead(statusCode);
-            res.end(payloadString);
-
-            console.log('Response vraca ' + statusCode + " " + payloadString);
-        });
-
-    });
-};
-
-const router = {
-    'ping': handlers.ping,
-    'users': handlers.users,
-    'tokens': handlers.tokens,
-    'checks': handlers.checks
 }
+
+app.init();
+
+module.exports = app;
